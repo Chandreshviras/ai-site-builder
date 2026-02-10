b651603e6594909a4fda8a7a922cde27462e4ae8
Chandreshviras <100798136+Chandreshviras@users.noreply.github.com>
2026-02-10 22:42:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("SUPABASE_KEY EXISTS:", !!process.env.SUPABASE_ANON_KEY);
 import express from "express";
 import { createClient } from "@supabase/supabase-js";
 
